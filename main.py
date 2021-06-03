0141aa33814495cc3a51d7a5034623f4cdb03a4d
Marawan Mamdouh <marawanmamdouh940@gmail.com>
2021-06-03 09:08:47 +0200

    clean and rearrange code

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
-import PyPDF2
 import difflib
-import docx
 import re
+
+import PyPDF2
+import docx
+
+words = []
 
 
 def hasNumbers(inputString):
     return any(char.isdigit() for char in inputString)
+
+
+def listWords(inputString):
+    inputString = re.sub('[^A-Za-z\']+', ' ', inputString)
+    for word in inputString.strip().split():
+        word = word.lower()
+        if word != "" and not hasNumbers(word):
+            words.append(word)
 
 
 def readDictionaryFile(fileName):
@@ -20,58 +31,40 @@ def readDictionaryFile(fileName):
 
 
 def readTextFile(fileName):
-    words = []
     extension = fileName.split(".")
     if extension[-1] == "pdf":
         file = open(fileName, "rb")
-        object = PyPDF2.PdfFileReader(file)
-        for i in range(object.getNumPages()):
-            page = object.getPage(i)
-            page_content = page.extractText()
-            for line in page_content.split():
-                line = re.sub('[^A-Za-z]+', ' ', line)
-                lineWords = line.strip().split()
-                for word in lineWords:
-                    word = word.lower()
-                    if word != "" and not hasNumbers(word):
-                        words.append(word)
+        pdfObject = PyPDF2.PdfFileReader(file)
+        for i in range(pdfObject.getNumPages()):
+            page = pdfObject.getPage(i)
+            fullText = str(page.extractText())
+            listWords(fullText)
         file.close()
     elif extension[-1] == "docx":
-        doc = docx.Document(fileName)
+        docObject = docx.Document(fileName)
         fullText = ""
-        for para in doc.paragraphs:
+        for para in docObject.paragraphs:
             fullText += " " + para.text
-        for line in fullText.split():
-            line = re.sub('[^A-Za-z]+', ' ', line)
-            lineWords = line.strip().split()
-            for word in lineWords:
-                word = word.lower()
-                if word != "" and not hasNumbers(word):
-                    words.append(word)
-    else:
+            listWords(fullText)
+    elif extension[-1] == "txt":
         file = open(fileName, "r")
         for line in file:
-            line = re.sub('[^A-Za-z]+', ' ', line)
-            lineWords = line.strip().split()
-            for word in lineWords:
-                word = word.lower()
-                if word != "" and not hasNumbers(word):
-                    words.append(word)
+            listWords(line)
         file.close()
     return list(set(words))
 
 
-def findErrors(dictionaryList, textlist):
+def findErrors(dictionaryWordsList, inputTextList):
     misspellings = []
-    for word in textlist:
-        if word not in dictionaryList:
+    for word in inputTextList:
+        if word not in dictionaryWordsList:
             misspellings.append(word)
     return list(set(misspellings))
 
 
-def suggest(errorList, dictionary):
-    for word in errorList:
-        suggestion = difflib.get_close_matches(word, dictionary)
+def suggest(misspellings, dictionaryWordsList):
+    for word in misspellings:
+        suggestion = difflib.get_close_matches(word, dictionaryWordsList)
         print(f"- Did you mean {', '.join(str(x) for x in suggestion)} instead of {word}?")
 
 
@@ -85,34 +78,43 @@ print('''
 ───────────────────────────────────────────║║
 ───────────────────────────────────────────╚╝
 ''')
-inputType = input("Enter (1) to check file or Enter (2) to write some words to check : ")
-if inputType == "1":
-    dictionaryFile = input("Please enter the dictionary file : ")
-    textFile = input("Please enter the text file : ")
-    dictionaryList = readDictionaryFile(dictionaryFile.strip())
-    textlist = readTextFile(textFile.strip())
-    errorList = findErrors(dictionaryList, textlist)
-    print("errorList -> ", errorList)
-    suggest(errorList, dictionaryList)
-elif inputType == "2":
-    words = []
-    dictionaryList = readDictionaryFile("dictionary.txt".strip())
-    text = input("write text to check : \n")
-    for line in text.split():
-        line = re.sub('[^A-Za-z-]+', ' ', line)
-        lineWords = line.strip().split()
-        for word in lineWords:
-            word = word.lower()
-            if word != "" and not hasNumbers(word):
-                words.append(word)
-    errorList = findErrors(dictionaryList, list(set(words)))
-    if not errorList:
-        print("There is No Errors (^‿^)\n")
-    else:
-        print("errorList -> ", errorList)
-        suggest(errorList, dictionaryList)
+misspellings = []
+while 1:
+    inputType = input("Enter (1) to check file or Enter (2) to write some words to check : ")
+    if inputType == "1":
+        while 1:
+            dictionaryFileName = input("Please enter the dictionary file (.txt) : ").strip()
+            if dictionaryFileName.split(".")[-1] == "txt":
+                break
+            else:
+                print("\nError Enter .txt file")
+        while 1:
+            textFileName = input("Please enter the text file (.txt, .pdf or .docx) : ").strip()
+            if textFileName.split(".")[-1] == "txt" or textFileName.split(".")[-1] == "pdf" or \
+                    textFileName.split(".")[-1] == "docx":
+                break
+            else:
+                print("\nError Enter (.txt, .pdf or .docx) file")
+        dictionaryWordsList = readDictionaryFile(dictionaryFileName.strip())
+        inputTextList = readTextFile(textFileName.strip())
+        misspellings = findErrors(dictionaryWordsList, inputTextList)
+    elif inputType == "2":
+        dictionaryWordsList = readDictionaryFile("dictionary.txt".strip())
+        text = input("write text to check : \n")
+        listWords(text)
+        misspellings = findErrors(dictionaryWordsList, list(set(words)))
 
-    print('''┏━━━━┓┏┓━━━━━━━━━━━┏┓━━━━━━━━━━━┏━┓━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┏┓━┏┓━━━━━━━━━━━━━━━━━━━━━━┏┓━┏┓━━━━━━━━━┏┓━━━━━━━━━━┏┓━━━━━━━━━
+    if inputType == "1" or inputType == "2":
+        if not misspellings:
+            print("\nThere is No misspellings (^‿^)\n")
+        else:
+            print("\nmisspellings -> ", misspellings, "\n")
+            suggest(misspellings, dictionaryWordsList)
+        break
+    else:
+        print("\nPlease Enter Correct Choice", "(1) to check file or (2) to write some words to check")
+
+print('''\n\n┏━━━━┓┏┓━━━━━━━━━━━┏┓━━━━━━━━━━━┏━┓━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┏┓━┏┓━━━━━━━━━━━━━━━━━━━━━━┏┓━┏┓━━━━━━━━━┏┓━━━━━━━━━━┏┓━━━━━━━━━
 ┃┏┓┏┓┃┃┃━━━━━━━━━━━┃┃━━━━━━━━━━━┃┏┛━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┏┛┗┓┃┃━━━━━━━━━━━━━━━━━━━━━━┃┃━┃┃━━━━━━━━━┃┃━━━━━━━━━━┃┃━━━━━━━━━
 ┗┛┃┃┗┛┃┗━┓┏━━┓━┏━┓━┃┃┏┓┏━━┓━━━━┏┛┗┓┏━━┓┏━┓━━━━┏┓┏┓┏━━┓┏┓┏━┓━┏━━┓━━━━┗┓┏┛┃┗━┓┏━━┓━━━━┏━━┓┏━━┓┏━━┓┃┃━┃┃━━━━━┏━━┓┃┗━┓┏━━┓┏━━┓┃┃┏┓┏━━┓┏━┓
 ━━┃┃━━┃┏┓┃┗━┓┃━┃┏┓┓┃┗┛┛┃━━┫━━━━┗┓┏┛┃┏┓┃┃┏┛━━━━┃┃┃┃┃━━┫┣┫┃┏┓┓┃┏┓┃━━━━━┃┃━┃┏┓┃┃┏┓┃━━━━┃━━┫┃┏┓┃┃┏┓┃┃┃━┃┃━━━━━┃┏━┛┃┏┓┃┃┏┓┃┃┏━┛┃┗┛┛┃┏┓┃┃┏┛
